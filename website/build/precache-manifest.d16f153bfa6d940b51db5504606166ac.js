9481aaddadd5872ed6a43b40c9e4fd5f1d7cbd71
Desire Arra <desirearra@gmail.com>
2020-02-23 10:41:31 +0100

    static homepage build

---
+++ website/build/precache-manifest.d16f153bfa6d940b51db5504606166ac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358fba5735d9b9f6d8302a9418e22669",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c697a5c8f7c731781359",
+    "url": "/static/css/2.69989f4b.chunk.css"
+  },
+  {
+    "revision": "52984b9450dc9b72d2d6",
+    "url": "/static/css/main.fcaccacd.chunk.css"
+  },
+  {
+    "revision": "c697a5c8f7c731781359",
+    "url": "/static/js/2.4e3ec92c.chunk.js"
+  },
+  {
+    "revision": "52984b9450dc9b72d2d6",
+    "url": "/static/js/main.a4dea64b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc15c272ac709ac76e60d0898f65c3b6",
+    "url": "/static/media/avatar.fc15c272.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
